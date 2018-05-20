b426fe1cf199895a21ace28019b1b7f87bce2151
utsavpatel51 <utsavpatel51@gmail.com>
2018-05-20 15:39:49 +0530

    Add files via upload

--- score.py
+++ score.py
@@ -1,11 +1,13 @@
+import sys
 import requests
+from win10toast import ToastNotifier
 from bs4 import BeautifulSoup
-
+t=ToastNotifier()
 def main():
     url= "https://synd.cricbuzz.com/j2me/1.0/livematches.xml"
     r=requests.get(url)
     soup = BeautifulSoup(r.text,"html.parser")
-    #print(soup)
+    print(soup)
     matches=soup.find_all('match')
     info=[]
     for i in matches:
@@ -36,6 +38,12 @@ def teaminfo(team):
     e['over2']=team.blgtm.inngs['ovrs']
     e['score2']=team.blgtm.inngs['r']
     e['wicket2']=team.blgtm.inngs['wkts']
+    s1=e['1st team']+"_"+e['over1']+"_"+e['score1']+"_"+e['wicket1']
+    s2=e['2nd team']+"_"+e['over2']+"_"+e['score2']+"_"+e['wicket2']
+    s1=str(s1)
+    s2=str(s2)
+    t.show_toast(s1,duration=5)
+    t.show_toast(s2,duration=5)
     return e
 if __name__=="__main__":
     main()
